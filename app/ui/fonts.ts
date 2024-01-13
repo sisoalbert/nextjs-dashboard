ef896121c1d71757242690e31c1acdc4dc314a52
Siso Ngqolosi <sisoalbert@yahoo.com>
2024-01-13 23:16:00 +0200

    nextjs dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+//export new font Lusitana
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
