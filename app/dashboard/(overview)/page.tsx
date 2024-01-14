4a92ce5b593b0da1238caec6abaa21cc8a6b4e03
G <B>
2024-01-14 12:13:57 +0200

    improved accessibility in forms with React Form Status and Server-side Validation

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices } from '../../lib/data';
 import { Suspense } from 'react';
 import {
   CardSkeleton,
@@ -11,13 +10,6 @@ import {
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 
 export default async function Page() {
-  const {
-    totalPaidInvoices,
-    totalPendingInvoices,
-    numberOfInvoices,
-    numberOfCustomers,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
